fddf262b711db2042322006a6f20de8781017faf
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2022-03-06 23:06:07 +0900

    ArrayListに変換

--- src/iterator/BookShelf.java
+++ src/iterator/BookShelf.java
@@ -1,28 +1,56 @@
 package iterator;
 
+import java.util.ArrayList;
+
+//public class BookShelf implements Aggregate {
+//
+//	private Book[] books;
+//	private int last = 0;
+//
+//	public BookShelf(int maxsize) {
+//		this.books = new Book[maxsize];
+//	}
+//	public Book getBookAt(int index) {
+//		return books[index];
+//	}
+//
+//	public void appendBook(Book book) {
+//
+//		this.books[last] = book;
+//		last++;
+//	}
+//
+//	public int getLength() {
+//		return last;
+//	}
+//
+//	public Iterator iterator() {
+//		return new BookShelfIterator(this);
+//	}
+//}
+
 public class BookShelf implements Aggregate {
 
-	private Book[] books;
-	private int last = 0;
+	private ArrayList<Book> books;
 
-	public BookShelf(int maxsize) {
-		this.books = new Book[maxsize];
+	public BookShelf(int... a) {
+		 books = new ArrayList<Book>();
 	}
+
 	public Book getBookAt(int index) {
-		return books[index];
+		return books.get(index);
 	}
 
 	public void appendBook(Book book) {
-
-		this.books[last] = book;
-		last++;
+		books.add(book);
 	}
 
 	public int getLength() {
-		return last;
+		return books.size();
 	}
 
 	public Iterator iterator() {
 		return new BookShelfIterator(this);
 	}
+
 }
